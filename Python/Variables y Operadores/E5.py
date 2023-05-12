0a7d75adf8a8a42233002e3a3579eb58f3c11411
elliotmc02 <elliotmoyanocutler2002@gmail.com>
2023-05-12 11:15:44 +0200

    E5 python

--- Python/Variables y Operadores/E5.py
+++ Python/Variables y Operadores/E5.py
@@ -3,3 +3,4 @@ Diseña un algoritmo que pida por teclado un tiempo expresado en segundos y
 muestre por pantalla ese valor expresado en horas, minutos y segundos.
 """
 seg = int(input("Dame los segundos: "))
+print("Horas:", int(seg / 3600), "Minutos:", int((seg % 3600) / 60), "Segundos:", int((seg % 3600) % 60))
---
+++ Python/Variables y Operadores/E6.py
@@ -0,0 +1,6 @@
+"""
+Crea un algoritmo que, partiendo de la cadena “LA LLUVIA EN SEVILLA ES UNA
+MARAVILLA” obtenga las cadenas “sevilla es una maravilla” y “la lluvia es
+una maravilla”. Muestra ambas por pantalla.
+"""
+cadena = "LA LLUVIA EN SEVILLA ES UNA MARAVILLA"
